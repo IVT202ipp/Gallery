28567abd8703c6dc603266c17a59cfbc1ae6f031
IVT202ipp <dg_gd_84@mail.ru>
2023-07-04 16:03:09 +0900

    fixes

--- components/Screens/Gallery.tsx
+++ components/Screens/Gallery.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { View, FlatList, Image, StyleSheet, Dimensions, TouchableOpacity, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../Redux/imageSlice';
 import { Loading } from '../Loading';
+import { GalleryStyle } from '../GalleryStyle';
 
 export const Gallery = ({ navigation } : {navigation: any}) => {
     const dispatch = useDispatch<any>();
@@ -20,47 +20,9 @@ export const Gallery = ({ navigation } : {navigation: any}) => {
     }
     
     return (
-    <View style={styles.container}>
-        <FlatList
-        data={products}
-        renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => navigation.navigate('FSImage', { item })}>
-                <View style={styles.imageContainer}>
-                    <Image source={{ uri: item.thumbnail }} style={styles.image} />
-                    {item.isFavorite && <Text style={styles.favoriteIcon}>★</Text>}
-                </View>
-            </TouchableOpacity>
-        )}
-        numColumns={3}
-        />
-    </View>
+      <GalleryStyle products={products} navigation={navigation} />
     );
 };
-
-const { width } = Dimensions.get('window');
-const imageWidth = width / 3 - 16;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  image: {
-    width: imageWidth,
-    height: imageWidth,
-    margin: 8,
-  },
-  imageContainer: {
-    position: 'relative',
-  },
-  favoriteIcon: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-    color: 'gold',
-    fontSize: 20,
-  },
-});
-
 
 
 
